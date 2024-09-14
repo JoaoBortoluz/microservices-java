337cb64da28b35039629a9e909c193c1be8cafac
JoaoV <JoaoV@ASUSVIVOBOOK15>
2024-09-13 23:06:52 -0300

    ADD produto-service

---
+++ produto-service/src/test/java/br/edu/atitus/paradigma/produto_server/ProdutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.paradigma.produto_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
